ae7a48475f3460c589faf57b4b64b976c3362d31
LukeMartindale <luke.martindale@hotmail.co.uk>
2022-10-27 16:46:48 +0100

    Added Url to Allowed Hosts & added enviro variable

--- steam_vision/settings.py
+++ steam_vision/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x(*po_*$9%@qdo=@qz%--qt*=qd%u*(cngs_k(iohczefziymy'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['steam-vision.herokuapp.com']
 
 
 # Application definition
