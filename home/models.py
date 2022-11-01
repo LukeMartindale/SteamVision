0f96ae5fb37391ff37bf5f916a8433ecc3bda57a
LukeMartindale <luke.martindale@hotmail.co.uk>
2022-11-01 09:31:24 +0000

    Initiale Models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    title = models.TextField()
+
+
+class Review(models.Model):
+    steam_id = models.ForeignKey(Game, on_delete=models.CASCADE)
