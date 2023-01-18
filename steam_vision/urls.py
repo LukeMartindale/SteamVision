1505ecef0efc46b27336898c755eea2806c86f53
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-01-18 21:28:13 +0000

    Started User Register Page

--- steam_vision/urls.py
+++ steam_vision/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('users/', include('users.urls')),
     path('', include('home.urls')),
     path('charts/', include('charts.urls')),
 
