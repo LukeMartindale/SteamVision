5d106335d00cf4c25681e42ab355b3c51996679a
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-01-18 12:36:57 +0000

    debug prints

--- data_collectors/reviews_collector.py
+++ data_collectors/reviews_collector.py
@@ -23,10 +23,13 @@ def initial_reviews_collector(game_id):
                 print("TEST 1")
                 review.app_id = game.first()
                 print("TEST app_id")
+                print(review.app_id)
                 review.review_id = response_reviews["reviews"][i]["recommendationid"]
                 print("TEST review_id")
+                print(review.review_id)
                 review.author_id = response_reviews["reviews"][i]["author"]["steamid"]
                 print("TEST author_id")
+                print(review.author_id)
 
                 print("TEST 2")
                 review.language = response_reviews["reviews"][i]["language"]
@@ -35,12 +38,20 @@ def initial_reviews_collector(game_id):
                 print("TEST 3")
                 review.time_created = datetime.fromtimestamp(response_reviews["reviews"][i]["timestamp_created"])
                 print("TEST time created")
+                print(review.time_created)
                 if response_reviews["reviews"][i]["author"].get("playtime_at_review"):review.playtime_at_review = response_reviews["reviews"][i]["author"]["playtime_at_review"]
                 print("TEST playtime at review")
+                print(review.playtime_at_review)
 
                 review.voted_up = response_reviews["reviews"][i]["voted_up"]
+                print("TEST voted up")
+                print(review.voted_up)
                 review.votes_up = response_reviews["reviews"][i]["votes_up"]
+                print("TEST votes up")
+                print(review.voted_up)
                 review.votes_funny = response_reviews["reviews"][i]["votes_funny"]
+                print("TEST votes funny")
+                print(review.votes_funny)
 
                 review.purchase_on_steam = response_reviews["reviews"][i]["steam_purchase"]
                 review.received_for_free = response_reviews["reviews"][i]["received_for_free"]
