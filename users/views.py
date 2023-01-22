b43e2d1c6c42a71f4fd859e6652a4a7846939504
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-01-22 17:02:54 +0000

    Basic Login Functionality Implemented

--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account Created!")
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+def loginPage(request):
+    return render(request, 'users/login.html')
