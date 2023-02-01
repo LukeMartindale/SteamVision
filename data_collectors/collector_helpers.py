056c288e3980232a0fcb1d37e2b99db8f3c124d2
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-02-01 19:08:00 +0000

    get tags and new descriptors

--- data_collectors/collector_helpers.py
+++ data_collectors/collector_helpers.py
@@ -1,4 +1,6 @@
 import datetime
+from bs4 import BeautifulSoup
+from home.models import Descriptor
 
 def convertDate(string_date):
 
@@ -16,3 +18,40 @@ def descriptionStripper(items):
     for item in items:
         descriptions.append(item['description'])
     return descriptions
+
+def tagStripper(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    html = soup.find_all('div', {"class": "app_tag"})
+
+    tags = []
+
+    for tag in html:
+        tags.append(tag.get_text())
+
+    return tags
+
+def newDescriptors(descriptors_genre, descriptors_tags, descriptors_categories):
+
+    for genre in descriptors_genre:
+        if not Descriptor.objects.filter(type="Genres", name=genre).exists():
+            print(genre)
+            desc = Descriptor()
+            desc.type = "Genres"
+            desc.name = genre
+            desc.save()
+
+    for tag in descriptors_tags:
+        if not Descriptor.objects.filter(type="Tags", name=tag).exists():
+            print(tag)
+            desc = Descriptor()
+            desc.type = "Tags"
+            desc.name = tag
+            desc.save()
+
+    for category in descriptors_categories:
+        if not Descriptor.objects.filter(type="Categories", name=category).exists():
+            print(category)
+            desc = Descriptor()
+            desc.type = "Categories"
+            desc.name = category
+            desc.save()
--- data_collectors/game_collector.py
+++ data_collectors/game_collector.py
@@ -1,13 +1,26 @@
-from data_collectors.collector_helpers import convertDate, descriptionStripper
+from data_collectors.collector_helpers import convertDate, descriptionStripper, tagStripper, newDescriptors
 
 from home.models import Game
 import requests
+from bs4 import BeautifulSoup
 
 def game_collector(game_id):
     api_url = "http://store.steampowered.com/api/appdetails?appids={}&cc=UK".format(game_id)
     response_app = requests.get(api_url).json()
 
-    game = Game()
+    tag_url = "https://store.steampowered.com/apphoverpublic/{}".format(game_id)
+    response_tags = requests.get(tag_url)
+
+    tags = tagStripper(response_tags)
+
+    newDescriptors(descriptionStripper(response_app[game_id]["data"]["genres"]), tags, descriptionStripper(response_app[game_id]["data"]["categories"]))
+
+    if(Game.objects.filter(app_id=game_id).exists()):
+        game = Game.objects.get(app_id=game_id)
+    else:
+        game = Game()
+
+
 
     game.app_id = response_app[game_id]["data"]["steam_appid"]
     game.name = response_app[game_id]["data"]["name"]
@@ -18,6 +31,7 @@ def game_collector(game_id):
     game.publisher = response_app[game_id]["data"]["publishers"]
 
     game.genres =  descriptionStripper(response_app[game_id]["data"]["genres"])
+    game.tags = tags
     game.categories = descriptionStripper(response_app[game_id]["data"]["categories"])
     game.supported_languages = response_app[game_id]["data"]["supported_languages"]
 
