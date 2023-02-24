da41b49c5af67c114812e9c21fe8b6efbb881d41
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-02-24 19:26:20 +0000

    option to run all app_sentiment process

--- home/management/commands/app_sentiment.py
+++ home/management/commands/app_sentiment.py
@@ -14,25 +14,32 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--id")
         parser.add_argument("--time")
+        parser.add_argument("--process")
 
     def handle(self, *args, **options):
-        if options["time"].lower() == "all-time" or None:
-            if(options["id"].lower() == "all"):
-                app_sentiment_all_time_month_all()
-            else:
-                app_sentiment_all_time_month(options["id"])
-        elif options["time"].lower() == "one-month":
-            if(options["id"].lower() == "all"):
-                app_sentiment_past_one_month_all()
-            else:
-                app_sentiment_past_one_month(options["id"])
-        elif options["time"].lower() == "two-weeks":
-            if(options["id"].lower() == "all"):
-                app_sentiment_past_two_weeks_all()
-            else:
-                app_sentiment_past_two_weeks(options["id"])
-        elif options["time"].lower() == "one-week":
-            if(options["id"].lower() == "all"):
-                app_sentiment_past_one_week_all()
-            else:
-                app_sentiment_past_one_week(options["id"])
+        if options["process"].lower() == "true":
+            app_sentiment_all_time_month_all()
+            app_sentiment_past_one_month_all()
+            app_sentiment_past_two_weeks_all()
+            app_sentiment_past_one_week_all()
+        else:
+            if options["time"].lower() == "all-time" or None:
+                if(options["id"].lower() == "all"):
+                    app_sentiment_all_time_month_all()
+                else:
+                    app_sentiment_all_time_month(options["id"])
+            elif options["time"].lower() == "one-month":
+                if(options["id"].lower() == "all"):
+                    app_sentiment_past_one_month_all()
+                else:
+                    app_sentiment_past_one_month(options["id"])
+            elif options["time"].lower() == "two-weeks":
+                if(options["id"].lower() == "all"):
+                    app_sentiment_past_two_weeks_all()
+                else:
+                    app_sentiment_past_two_weeks(options["id"])
+            elif options["time"].lower() == "one-week":
+                if(options["id"].lower() == "all"):
+                    app_sentiment_past_one_week_all()
+                else:
+                    app_sentiment_past_one_week(options["id"])
