9f4d7efafbebe37809b8bb4575ae54c8c4e4b2ae
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-02-26 18:22:04 +0000

    Added check for when no data for emotion timeframe

--- home/static/home/javascript/visualisations/emotion/emotion-all-time.js
+++ home/static/home/javascript/visualisations/emotion/emotion-all-time.js
@@ -27,62 +27,72 @@ function emotion_all_time(id){
     let emotion = EmotionSection(raw_data, ["#0000FF","#0052FF","#007AFF","#00A3FF","#00CCFF"])
     let reviews_data = emotion.section_data
 
-    $('#emotion-container-content').empty()
+    if(emotion.status){
 
-    //set the dimensions and margins of the graph
-    let margin = 40
-    let svgWidth = $('#emotion-container-content').width()
-    let svgHeight = $('#emotion-container-content').height()
+        $('#emotion-container-content').empty()
 
-    //radius of the pieplot is half the width or half the height(smallest one)
-    let radius = Math.min(svgWidth, svgHeight) / 2 - margin
-
-    //appened svg to div
-    let svg = d3.select("#emotion-container-content")
-        .append("svg")
-            .attr("width", svgWidth)
-            .attr("height", svgHeight)
-        .append("g")
-            .attr("transform", `translate(${svgWidth/2}, ${svgHeight/2})`);
-
-    //set the colour scale
-    let colour = d3.scaleOrdinal()
-        .range(emotion.section_colour)
-
-    //compute poisition of each group in the chart
-    let pie = d3.pie()
-        .value(function(data) {return data[1];})
-        .sort(null)
-    let data_ready = pie(Object.entries(reviews_data))
-
-    //generates arcs, used for adding the notation to the graph
-    let arcGenerator = d3.arc()
-        .innerRadius(0)
-        .outerRadius(radius)
-
-    svg
-        .selectAll("slices")
-        .data(data_ready)
-        .join('path')
-        .attr('d', d3.arc()
+        //set the dimensions and margins of the graph
+        let margin = 40
+        let svgWidth = $('#emotion-container-content').width()
+        let svgHeight = $('#emotion-container-content').height()
+    
+        //radius of the pieplot is half the width or half the height(smallest one)
+        let radius = Math.min(svgWidth, svgHeight) / 2 - margin
+    
+        //appened svg to div
+        let svg = d3.select("#emotion-container-content")
+            .append("svg")
+                .attr("width", svgWidth)
+                .attr("height", svgHeight)
+            .append("g")
+                .attr("transform", `translate(${svgWidth/2}, ${svgHeight/2})`);
+    
+        //set the colour scale
+        let colour = d3.scaleOrdinal()
+            .range(emotion.section_colour)
+    
+        //compute poisition of each group in the chart
+        let pie = d3.pie()
+            .value(function(data) {return data[1];})
+            .sort(null)
+        let data_ready = pie(Object.entries(reviews_data))
+    
+        //generates arcs, used for adding the notation to the graph
+        let arcGenerator = d3.arc()
             .innerRadius(0)
             .outerRadius(radius)
-        )
-        .attr('fill', function(d){return(colour(d.data[1])) })
-        .style("stroke-width", "2px")
-        .style("opacity", 0.7)
+    
+        svg
+            .selectAll("slices")
+            .data(data_ready)
+            .join('path')
+            .attr('d', d3.arc()
+                .innerRadius(0)
+                .outerRadius(radius)
+            )
+            .attr('fill', function(d){return(colour(d.data[1])) })
+            .style("stroke-width", "2px")
+            .style("opacity", 0.7)
+    
+        //add labels to the chart
+        svg
+            .selectAll("slices")
+            .data(data_ready)
+            .join('text')
+            .text(function(data){ return(data.data[0]) })
+            .attr("transform", function(data) { return `translate(${arcGenerator.centroid(data)})` })
+            .style("text-anchor", "middle")
+            .style("font-size", 17)
+            .style("font-weight","bold")
+            .style("text-transform", "uppercase")
+            .style("fill", "#bec5cb")
 
-    //add labels to the chart
-    svg
-        .selectAll("slices")
-        .data(data_ready)
-        .join('text')
-        .text(function(data){ return(data.data[0]) })
-        .attr("transform", function(data) { return `translate(${arcGenerator.centroid(data)})` })
-        .style("text-anchor", "middle")
-        .style("font-size", 17)
-        .style("font-weight","bold")
-        .style("text-transform", "uppercase")
-        .style("fill", "#bec5cb")
+    } else {
+
+        $('#emotion-container-content').empty()
+        $('#emotion-container-content').append('<div class="visualisation-container-content-no-data">No data for this timeframe!</div>')
+    }
+
+
 
 }
--- home/static/home/javascript/visualisations/emotion/emotion-visualisation-helper.js
+++ home/static/home/javascript/visualisations/emotion/emotion-visualisation-helper.js
@@ -3,17 +3,19 @@ function EmotionSection(data, colour){
     let section_data = {}
     let section_colour = []
     let index = 0
+    let status = false
 
     for(let [emotion, value] of Object.entries(data)){
         obj = {}
         if(value > 0){
+            status = true
             section_data[emotion] = value
             section_colour.push(colour[index])
         }
         index++
     }
 
-    return {"section_data": section_data, "section_colour": section_colour}
+    return {"section_data": section_data, "section_colour": section_colour, "status": status}
 }
 
 
