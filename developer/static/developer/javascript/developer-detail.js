27732d01e77cbbb5a2945d828b4ea0a063588b83
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-03-06 15:24:28 +0000

    developer games list links to game page

--- developer/static/developer/javascript/developer-detail.js
+++ developer/static/developer/javascript/developer-detail.js
@@ -1,5 +1,8 @@
 $(function(){
 
     console.log("DEVELOPER DETAIL LOAD TEST")
+    $(".developer-game-widget-wrapper").click(function(){
+        window.location.href = '/games/' + $(this).attr('id').split("-")[2]
+    })
     
 })
