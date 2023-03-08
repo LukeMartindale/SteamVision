c7767a502cd96840850aac5484aaab6ee3db6df3
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-03-08 22:01:12 +0000

    added profile model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
+class Profile(models.Model):
+    
+    #user
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
+    #followed_games
+    followed_games = models.JSONField(default=dict)
+
+    #system_info
+    system_info = models.JSONField(default=dict)
