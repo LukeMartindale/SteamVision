be530a7befc26212c73686096dca130233421972
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-03-08 15:35:51 +0000

    charts page mainats choice thorugh pagination

--- charts/views.py
+++ charts/views.py
@@ -12,13 +12,12 @@ def Charts(request):
     games_sorted = []
     games_subject = "player_count"
     games_type = "current"
+    pass_search = False
 
     if request.method == 'POST':
-        print("POST")
+        pass_search = True
+
         if(request.POST["subject"] == "player-count"):
-
-            print(request.POST["subject"])
-
             # Get List of current most played games
             for game in games:
 
@@ -34,9 +33,6 @@ def Charts(request):
             games_type = "current"
             
         elif (request.POST["subject"] == "reviews"):
-
-            print(request.POST["subject"])
-
             # Get List of current most played games
             for game in games:
 
@@ -51,8 +47,39 @@ def Charts(request):
             games_subject = "reviews"
             games_type = "current"
 
+    elif request.GET.get('subject', False) or request.GET.get('type', False):
+        pass_search = True
+
+        if(request.GET.get('subject', False) == "player_count"):
+            # Get List of current most played games
+            for game in games:
+
+                current = {}
+                player_count = PlayerCount.objects.filter(app_id=game).last()
+
+                current["app"] = game
+                current["player_count"] = player_count.player_count
+                games_sorted.append(current)
+
+            games_sorted = sorted(games_sorted, key=lambda x:x['player_count'], reverse=True)
+            games_subject = "player_count"
+            games_type = "current"
+
+        elif(request.GET.get('subject', False) == "reviews"):
+            # Get List of current most played games
+            for game in games:
+
+                current = {}
+                review_percentage = GameStat.objects.get(app_id=game).current_review_score
+
+                current["app"] = game
+                current["reviews_percentage"] = review_percentage
+                games_sorted.append(current)
+
+            games_sorted = sorted(games_sorted, key=lambda x:x['reviews_percentage'], reverse=True)
+            games_subject = "reviews"
+            games_type = "current"
     else:
-        print("ELSE")
         # Get List of current most played games
         for game in games:
 
@@ -70,6 +97,6 @@ def Charts(request):
     paginated_games = paginator.get_page(page_number)
     sub_num = 10*(paginated_games.number-1)
 
-    context = {"games": paginated_games, "games_subject": games_subject, "games_type": games_type, "sub_num": sub_num}
+    context = {"games": paginated_games, "games_subject": games_subject, "games_type": games_type, "sub_num": sub_num, "pass_search": pass_search}
 
     return render(request, 'charts/charts.html', context)
