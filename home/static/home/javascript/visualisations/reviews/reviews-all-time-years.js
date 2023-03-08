c5a7abb9dcf71c66b70a4344ce89cb850b0c2b21
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-03-08 16:21:16 +0000

    improvements to reviews all time graph

--- home/static/home/javascript/visualisations/reviews/reviews-all-time-years.js
+++ home/static/home/javascript/visualisations/reviews/reviews-all-time-years.js
@@ -26,9 +26,21 @@ function review_all_time_year(id){
 
     $('#reviews-graph').empty()
 
-    let margins = {top: 0, bottom: 0, left: 40, right: 20}
+    let margins = {top: 0, bottom: 0, left: 0, right: 0}
+
+    // MARGINS
+    if ($(window).width() <= 475) {
+
+        margins = {top: 25, bottom: 35, left: 25, right: 0}
+
+    } else {
+
+        margins = {top: 25, bottom: 35, left: 50, right: 0}
+
+    }
+
     let svgWidth = $('#reviews-container-content').width() - margins.left - margins.right
-    let svgHeight = 400 - margins.top - margins.bottom
+    let svgHeight = 440 - margins.top - margins.bottom
 
     let x = d3.scaleBand().rangeRound([0, svgWidth]).padding(0.1);
     let y = d3.scaleLinear().range([svgHeight, 0]);
@@ -43,27 +55,134 @@ function review_all_time_year(id){
 
     let chart = chartContainer
         .append('g')
-        .attr("transform", `translate(${margins.left})`);
+        .attr("transform", `translate(${margins.left},${margins.top})`);
         
-    //X-AXIS LABELS
-    chart
-        .append('g')
-        .call(d3.axisBottom(x).tickSizeOuter(0))
-        .attr('transform', `translate(0, ${svgHeight})`)
-        .attr('color', '#bec5cb')
-        .attr('font-size', 21)
-        .selectAll("text")  
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-65)");
+    if($(window).width() <= 800 && $(window).width() > 400){
+        //X-AXIS TICKS
+        chart
+            .append('g')
+            .call(d3.axisBottom(x).tickSizeOuter(5))
+            .attr('transform', `translate(0, ${svgHeight})`)
+            .attr('color', '#bec5cb')
+            .attr('font-size', 15)
+            .attr('font-weight', 'bold')
+            .selectAll("text")  
+
+    } else if ($(window).width() <= 400) {
+        //X-AXIS TICKS
+        chart
+            .append('g')
+            .call(d3.axisBottom(x).tickSizeOuter(5))
+            .attr('transform', `translate(0, ${svgHeight})`)
+            .attr('color', '#bec5cb')
+            .attr('font-size', 11)
+            .attr('font-weight', 'bold')
+            .selectAll("text")  
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-65)");
+    } else {
+        //X-AXIS TICKS
+        chart
+            .append('g')
+            .call(d3.axisBottom(x).tickSize(5))
+            .attr('transform', `translate(0, ${svgHeight})`)
+            .attr('color', '#bec5cb')
+            .attr('font-size', 15)
+            .attr('font-weight', 'bold');
+    }
+
+    // X-AXIS LABELS
+    if($(window).width() <= 800 && $(window).width() > 450){
+        // X AXIS LABELS 
+        chart
+            .append('text')
+            .attr('transform', `translate(${svgWidth/2 - margins.left + 30}, ${svgHeight + 58})`)
+            .attr('fill', '#bec5cb')
+            .attr('font-size', 20)
+            .attr('font-weight', 'bold')
+            .text("Year");
+    } else if ($(window).width() <= 450) {
+        // X AXIS LABELS 
+        chart
+            .append('text')
+            .attr('transform', `translate(${svgWidth/2 - margins.left + 10}, ${svgHeight + 58})`)
+            .attr('fill', '#bec5cb')
+            .attr('font-size', 15)
+            .attr('font-weight', 'bold')
+            .text("Year");
+    } else {
+        // X AXIS LABELS 
+        chart
+            .append('text')
+            .attr('transform', `translate(${svgWidth/2 - margins.left + 25}, ${svgHeight + 50})`)
+            .attr('fill', '#bec5cb')
+            .attr('font-size', 20)
+            .attr('font-weight', 'bold')
+            .text("Year");
+    }
     
-    //Y-AXIS LABELS
-    chart
-        .append('g')
-        .call(d3.axisLeft(y))
-        .attr('color', '#bec5cb')
-        .attr('font-size', 15);
+    // Y AXIS TICKS
+    if ($(window).width() <= 400) {
+        chart
+            .append('g')
+            .call(d3.axisLeft(y).tickSizeInner(-svgWidth).ticks(10))
+            .attr('color', '#bec5cb')
+            .attr('font-size', 12);
+    } else {
+        chart
+            .append('g')
+            .call(d3.axisLeft(y).tickSizeInner(-svgWidth).ticks(10))
+            .attr('color', '#bec5cb')
+            .attr('font-size', 15)
+    }
+
+    // Y AXIS LABELS
+    if ($(window).width() <= 475) {
+        chartContainer
+            .append('text')
+            .attr('y', -12.5)
+            .attr('x', -($("#sentiment-container-content").height()/1.75))
+            .attr("transform", "rotate(-90)")
+            .attr('fill', '#bec5cb')
+            .attr('font-size', 15)
+            .attr('font-weight', 'bold')
+            .text("Reviews Percentage");
+    } else {
+        chartContainer
+            .append('text')
+            .attr('y', 0)
+            .attr('x', -($("#sentiment-container-content").height()/1.75))
+            .attr("transform", "rotate(-90)")
+            .attr('fill', '#bec5cb')
+            .attr('font-size', 20)
+            .attr('font-weight', 'bold')
+            .text("Reviews Percentage");
+    }
+
+    // GRAPH TITLE
+    if ($(window).width() <= 475) {
+        // GRAPH TITLE
+        chartContainer
+            .append('text')
+            .attr('y', 12)
+            .attr('x', svgWidth/2.05)
+            .attr('fill', '#bec5cb')
+            .attr('font-size', 14)
+            .attr('font-weight', 'bold')
+            .text("All Time");
+    } else {
+        // GRAPH TITLE
+        chartContainer
+            .append('text')
+            .attr('y', 10)
+            .attr('x', svgWidth/1.92)
+            .attr('fill', '#bec5cb')
+            .attr('font-size', 16)
+            .attr('font-weight', 'bold')
+            .text("All Time");
+    }
 
     //NEG BARS
     chart
