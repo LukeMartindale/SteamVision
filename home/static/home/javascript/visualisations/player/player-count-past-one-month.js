1ff5b7eeb843dbd5df8ccec2de2d07222988ab29
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-04-22 22:51:24 +0100

    reduced thickness of certain player chart lines

--- home/static/home/javascript/visualisations/player/player-count-past-one-month.js
+++ home/static/home/javascript/visualisations/player/player-count-past-one-month.js
@@ -213,7 +213,7 @@ function player_count_past_one_month(id){
         .datum(player_data)
         .attr("fill", "none")
         .attr("stroke", "steelblue")
-        .attr("stroke-width", 4)
+        .attr("stroke-width", 2)
         .attr("d", d3.line()
             .x(function(data) {return x(data.timestamp)})
             .y(function(data) {return y(data.player_count)})
