c742354c1d9852eaf30c72202e3e9298442196ed
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-04-29 19:15:12 +0100

    sentiment widgets api endpoint

--- api/views.py
+++ api/views.py
@@ -604,17 +604,55 @@ def getVisualisationWidgetSentiementData(request, id):
     game = Game.objects.get(app_id=id)
     game_stats = GameStat.objects.get(app_id=game)
 
-    #     # All time
-    # all_time = {"peak", game_stats.highest_sentiment_score}
+    # All time
+    all_time = game_stats.highest_sentiment_score
     # # 12 months
-    # twelve_months_pos = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(weeks=52), timezone.now()], sentiment_polarity__gte=0.05000000000000001, sentiment_polarity__lte="")
-    # print(twelve_months_peak)
-    # # 6 months
-    # # 30 days
-    # # 2 weeks
-    # # 1 week
+    twelve_months_neu = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(weeks=52), timezone.now()], sentiment_polarity__gte=-0.05000000000000000, sentiment_polarity__lte=0.04999999999999999).count()
+    twelve_months_pos = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(weeks=52), timezone.now()], sentiment_polarity__gte=0.05000000000000001, sentiment_polarity__lte=1).count()
+    twelve_months_neg = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(weeks=52), timezone.now()], sentiment_polarity__gte=-1, sentiment_polarity__lte=-0.05000000000000001).count()
+    twelve_months_sub = twelve_months_pos + twelve_months_neg + twelve_months_neu
+    if twelve_months_sub > 0:
+        twelve_months = round((twelve_months_pos - twelve_months_neg) / twelve_months_sub, 2)
+    else:
+        twelve_months = 0
+    # 6 months
+    six_months_neu = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(weeks=26), timezone.now()], sentiment_polarity__gte=-0.05000000000000000, sentiment_polarity__lte=0.04999999999999999).count()
+    six_months_pos = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(weeks=26), timezone.now()], sentiment_polarity__gte=0.05000000000000001, sentiment_polarity__lte=1).count()
+    six_months_neg = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(weeks=26), timezone.now()], sentiment_polarity__gte=-1, sentiment_polarity__lte=-0.05000000000000001).count()
+    six_months_sub = six_months_pos + six_months_neg + six_months_neu
+    if six_months_sub > 0:
+        six_months = round((six_months_pos - six_months_neg) / six_months_sub, 2)
+    else:
+        six_months = 0
+    # 30 days
+    thrity_days_neu = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=30), timezone.now()], sentiment_polarity__gte=-0.05000000000000000, sentiment_polarity__lte=0.04999999999999999).count()
+    thrity_days_pos = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=30), timezone.now()], sentiment_polarity__gte=0.05000000000000001, sentiment_polarity__lte=1).count()
+    thrity_days_neg = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=30), timezone.now()], sentiment_polarity__gte=-1, sentiment_polarity__lte=-0.05000000000000001).count()
+    thrity_days_sub = thrity_days_pos + thrity_days_neg + thrity_days_neu
+    if thrity_days_sub > 0:
+        thrity_days = round((thrity_days_pos - thrity_days_neg) / thrity_days_sub, 2)
+    else:
+        thrity_days = 0
+    # 2 weeks
+    two_weeks_neu = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=14), timezone.now()], sentiment_polarity__gte=-0.05000000000000000, sentiment_polarity__lte=0.04999999999999999).count()
+    two_weeks_pos = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=14), timezone.now()], sentiment_polarity__gte=0.05000000000000001, sentiment_polarity__lte=1).count()
+    two_weeks_neg = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=14), timezone.now()], sentiment_polarity__gte=-1, sentiment_polarity__lte=-0.05000000000000001).count()
+    two_weeks_sub = two_weeks_pos + two_weeks_neg + two_weeks_neu
+    if two_weeks_sub > 0:
+        two_weeks = round((two_weeks_pos - two_weeks_neg) / two_weeks_sub, 2)
+    else:
+        two_weeks = 0
+    # 1 week
+    one_week_neu = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=7), timezone.now()], sentiment_polarity__gte=-0.05000000000000000, sentiment_polarity__lte=0.04999999999999999).count()
+    one_week_pos = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=7), timezone.now()], sentiment_polarity__gte=0.05000000000000001, sentiment_polarity__lte=1).count()
+    one_week_neg = Review.objects.filter(app_id__app_id=id, time_created__range=[timezone.now() - timezone.timedelta(days=7), timezone.now()], sentiment_polarity__gte=-1, sentiment_polarity__lte=-0.05000000000000001).count()
+    one_week_sub = one_week_pos + one_week_neg + one_week_neu
+    if one_week_sub > 0:
+        one_week = round((one_week_pos - one_week_neg) / one_week_sub, 2)
+    else:
+        one_week = 0
 
-    return Response({"message": "Sentiment Visualisation Widgets Data"})
+    return Response({"all_time": all_time, "twelve_months": twelve_months, "six_months": six_months, "thrity_days": thrity_days, "two_weeks": two_weeks, "one_week": one_week})
 
 @api_view(['GET'])
 def getVisualisationWidgetEmotionData(request, id):
