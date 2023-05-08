6f7b76d7b6144c0923b68bc44c5534e89a976a78
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-05-08 13:54:25 +0100

    minutes

--- home/static/home/javascript/visualisations/player/player-count-past-one-week.js
+++ home/static/home/javascript/visualisations/player/player-count-past-one-week.js
@@ -282,7 +282,11 @@ function player_count_past_one_week(id){
 
         let width_scale = 15
         let time = new Date(player_data[i].timestamp)
-        time = time.getDate() + "/" + time.getMonth() + "/" + time.getFullYear() + " - " + time.getHours() + ":" + time.getTime();
+        let minutes = time.getMinutes()
+        if (minutes < 10) {
+            minutes = "0" + minutes.toString()
+        }
+        time = time.getDate() + "/" + time.getMonth() + "/" + time.getFullYear() + " - " + time.getHours() + ":" + minutes;
 
         if(x(player_data[i].timestamp)+15 > $("#player-graph").width()/2){
             width_scale = -175
